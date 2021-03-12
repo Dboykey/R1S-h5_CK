fe678e09e911319c92efdec913e4c8842ce235b8
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-12 15:39:07 +0800

    Add files via upload

--- diy-p4.sh
+++ diy-p4.sh
@@ -12,12 +12,20 @@ make defconfig
 make download
 make tools/compile && \
 make toolchain/compile && \
-make package/feeds/luci/luci-base/compile && \
-cp dl/naiveproxy-*.tar.gz build_dir/target-aarch64_cortex-a53_musl/ && \
+make package/feeds/luci/luci-base/compile
+
+cd dl
+ls naiveproxy* >name.txt
+NAME1=$(cat name.txt)
+cat name.txt | sed -i 's/.tar.gz//g' name.txt
+NAME2=$(cat name.txt)
+cd ..
+cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/ && \
 cd ./build_dir/target-aarch64_cortex-a53_musl/ && \
-tar zxvf naiveproxy-*.tar.gz && \
-rm naiveproxy-*.tar.gz && \
+tar zxvf $NAME1 && \
+rm $NAME1 && \
 cd ../../ && \
-sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/naiveproxy-*/src/base/rand_util_posix.cc && \
+sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc && \
+
 #make -j1 V=s
-make -j1
+make -j3
