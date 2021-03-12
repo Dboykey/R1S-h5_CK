33161938e42a2170849cdc18aa934999ea824222
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-12 17:34:09 +0800

    Add files via upload

--- diy-p4.sh
+++ diy-p4.sh
@@ -14,19 +14,19 @@ make tools/compile
 make toolchain/compile
 make package/feeds/luci/luci-base/compile
 
-cd dl
-ls naiveproxy* >name.txt
-NAME1=$(cat name.txt)
-sed -i 's/.tar.gz//g' name.txt
-NAME2=$(cat name.txt)
-cd ..
-cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
-cd ./build_dir/target-aarch64_cortex-a53_musl/
-tar zxvf $NAME1
-rm $NAME1
-cd ../../
-sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc
-make package/passwall/naiveproxy/compile && \
-
-make -j1
-#make -j3
+#cd dl
+#ls naiveproxy* >name.txt
+#NAME1=$(cat name.txt)
+#sed -i 's/.tar.gz//g' name.txt
+#NAME2=$(cat name.txt)
+#cd ..
+#cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
+#cd ./build_dir/target-aarch64_cortex-a53_musl/
+#tar zxvf $NAME1
+#rm $NAME1
+#cd ../../
+#sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc
+#make package/passwall/naiveproxy/compile && \
+#
+#make -j1
+##make -j3
