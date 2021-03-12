fb31aabfb126bd2afeb52ae934763b96bf4df38e
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-12 15:43:25 +0800

    Add files via upload

--- diy-p4.sh
+++ diy-p4.sh
@@ -10,8 +10,8 @@ cd h5/friendlywrt
 cp $GITHUB_WORKSPACE/$CONFIG_FILE $GITHUB_WORKSPACE/openwrt/.config
 make defconfig
 make download
-make tools/compile && \
-make toolchain/compile && \
+make tools/compile
+make toolchain/compile
 make package/feeds/luci/luci-base/compile
 
 cd dl
@@ -20,12 +20,13 @@ NAME1=$(cat name.txt)
 cat name.txt | sed -i 's/.tar.gz//g' name.txt
 NAME2=$(cat name.txt)
 cd ..
-cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/ && \
-cd ./build_dir/target-aarch64_cortex-a53_musl/ && \
-tar zxvf $NAME1 && \
-rm $NAME1 && \
-cd ../../ && \
-sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc && \
+cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
+cd ./build_dir/target-aarch64_cortex-a53_musl/
+tar zxvf $NAME1
+rm $NAME1
+cd ../../
+sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc
+make package/passwall/naiveproxy/compile && \
 
-#make -j1 V=s
-make -j3
+make -j1
+#make -j3
