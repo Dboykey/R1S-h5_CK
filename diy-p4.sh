20e92cad79a1e9f0f648b0dbea72e479f593c659
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-13 10:08:32 +0800

    Update diy-p4.sh

--- diy-p4.sh
+++ diy-p4.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 #
 #############################
 ####  第四部分：编译WRT  ####
@@ -10,23 +10,25 @@ cd h5/friendlywrt
 cp $GITHUB_WORKSPACE/$CONFIG_FILE $GITHUB_WORKSPACE/openwrt/.config
 make defconfig
 make download
+
+mkdir -p build_dir/target-aarch64_cortex-a53_musl/
+cd dl
+ls naiveproxy* >name.txt
+##添加只取最后一行内容
+NAME1=$(cat name.txt)
+sed -i 's/.tar.gz//g' name.txt
+NAME2=$(cat name.txt)
+cd ..
+cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
+cd ./build_dir/target-aarch64_cortex-a53_musl/
+tar zxvf $NAME1
+rm $NAME1
+cd ../../
+sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc
+make package/passwall/naiveproxy/compile && \
 make tools/compile
 make toolchain/compile
 make package/feeds/luci/luci-base/compile
 
-#cd dl
-#ls naiveproxy* >name.txt
-#NAME1=$(cat name.txt)
-#sed -i 's/.tar.gz//g' name.txt
-#NAME2=$(cat name.txt)
-#cd ..
-#cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
-#cd ./build_dir/target-aarch64_cortex-a53_musl/
-#tar zxvf $NAME1
-#rm $NAME1
-#cd ../../
-#sed -i "s|sys/random.h|/usr/include/linux/random.h|g" build_dir/target-aarch64_cortex-a53_musl/$NAME2/src/base/rand_util_posix.cc
-#make package/passwall/naiveproxy/compile && \
-#
-#make -j1
-##make -j3
+make -j1
+#make -j3
