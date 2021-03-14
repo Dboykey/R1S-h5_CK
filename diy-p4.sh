2ff13d42e1fe903a06efbc46f5099f48d0d605aa
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-14 09:34:35 +0800

    Add files via upload

--- diy-p4.sh
+++ diy-p4.sh
@@ -13,28 +13,3 @@ make download
 make tools/compile
 make toolchain/compile
 make package/feeds/luci/luci-base/compile
-
-mkdir -p build_dir/target-aarch64_cortex-a53_musl/
-cd dl
-ls naiveproxy* >name1.txt
-##添加只取最后一行内容
-sed -n '$p' name1.txt >name2.txt
-#或
-#cat name1.txt | awk 'END {print}' >name2.txt
-NAME1=$(cat name2.txt)
-##删除“.tar.gz”字符串
-sed 's/.tar.gz//g' name2.txt >name3.txt
-NAME2=$(cat name3.txt)
-cd ..
-cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
-cd build_dir/target-aarch64_cortex-a53_musl/
-tar zxvf $NAME1
-rm $NAME1
-cd $NAME2/src/base/
-sed -i "s|sys/random.h|/usr/include/linux/random.h|g" rand_util_posix.cc
-
-cd $GITHUB_WORKSPACE/openwrt
-make package/passwall/naiveproxy/compile
-
-#make -j1
-make -j3 V=s
--- diy-p5.sh
+++ diy-p5.sh
@@ -1,34 +1,32 @@
 ﻿#!/bin/bash
 #
-#################################
-####  第五部分：生成R1S固件  ####
-#################################
+#############################
+####  第四部分：编译WRT  ####
+#############################
 
-cd h5
+# 编译 OpenWrt - naiveproxy
+cd openwrt
+#mkdir -p build_dir/target-aarch64_cortex-a53_musl/
+cd dl
+ls naiveproxy* >name1.txt
+##添加只取最后一行内容
+sed -n '$p' name1.txt >name2.txt
+#或
+#cat name1.txt | awk 'END {print}' >name2.txt
+NAME1=$(cat name2.txt)
+##删除“.tar.gz”字符串
+sed 's/.tar.gz//g' name2.txt >name3.txt
+NAME2=$(cat name3.txt)
+cd ..
+cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/
+cd build_dir/target-aarch64_cortex-a53_musl/
+tar zxvf $NAME1
+rm $NAME1
+cd $NAME2/src/base/
+sed -i "s|sys/random.h|/usr/include/linux/random.h|g" rand_util_posix.cc
 
-# 调整生成SD镜像的脚本
-cp scripts/build.sh scripts/build.sh.bak
+cd $GITHUB_WORKSPACE/openwrt
+make package/passwall/naiveproxy/compile
 
-# 删除把img用zip压缩
-sed -i '296,299d' scripts/build.sh
-
-# 调整输出R1S的脚本，删除重复编译wrt的步骤
-#sed -i '130,150 {/build_friendlywrt/d}' scripts/build.sh
-sed -i '130,150 {s/build_friendlywrt/#build_friendlywrt/}' scripts/build.sh
-
-# 为生成SD镜像的脚本加入gzip压缩功能
-sed -i "/space/a\\echo 'RAW image successfully compress'" scripts/sd-fuse/mk-sd-image.sh
-sed -i "/space/a\\gzip -9 {RAW_FILE}" scripts/sd-fuse/mk-sd-image.sh
-sed -i 's/gzip -9 /gzip -9 $/'  scripts/sd-fuse/mk-sd-image.sh
-sed -i "/space/a\\echo '---------------------------------'" scripts/sd-fuse/mk-sd-image.sh
-sed -i "/space/a\\ " scripts/sd-fuse/mk-sd-image.sh
-
-# 修改代码让其支持使用其他的wrt源码而不是特定的那套
-sed -i 's/root-allwinner-h5/root-sunxi/' device/friendlyelec/h5/base.mk
-
-# 正式生成SD镜像
-./build.sh nanopi_r1s.mk
-
-# 把生成的镜像复制到WRT的bin目录，方便上传到release
-rm $GITHUB_WORKSPACE/h5/friendlywrt/bin/targets/sunxi/cortexa53/openwrt*
-cp out/F*gz $GITHUB_WORKSPACE/openwrt/bin/targets/sunxi/cortexa53/
+#make -j1
+make -j3 V=s
