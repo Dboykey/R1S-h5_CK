2d8e9f30619c9d0b35759be54a7e88afd56681e0
Dboykey <58365695+Dboykey@users.noreply.github.com>
2021-03-18 20:49:52 +0800

    Add files via upload

--- diy-p5.sh
+++ diy-p5.sh
@@ -4,7 +4,21 @@
 ####  第四部分：编译WRT  ####
 #############################
 
-## 编译 OpenWrt
 cd openwrt
 
+cd dl
+ls naiveproxy* >name1.txt
+sed -n '$p' name1.txt >name2.txt
+NAME1=$(cat name2.txt)
+sed 's/.tar.gz//g' name2.txt >name3.txt
+NAME2=$(cat name3.txt)
+cd .. && \
+cp dl/$NAME1 build_dir/target-aarch64_cortex-a53_musl/ && \
+cd build_dir/target-aarch64_cortex-a53_musl/
+tar zxvf $NAME1 && \
+sleep 20s && \
+cd $NAME2/src/base/ && \
+sed -i "s|sys/random.h|/usr/include/linux/random.h|g" rand_util_posix.cc && \
+cd $GITHUB_WORKSPACE/openwrt
+make package/passwall/naiveproxy/compile && \
 make -j1 V=s
